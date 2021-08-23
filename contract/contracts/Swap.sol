c1c137ff2547d36fc6c592158c21a4cb2a2d10d3
Aurélien <aurelien.catinon@gmail.com>
2021-08-23 23:04:33 +0200

    Swap contract implementation continued

--- contract/contracts/Swap.sol
+++ contract/contracts/Swap.sol
@@ -1,24 +1,50 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract OfficialSwapInterface {
-    function swap(uint256 _amount) public view returns (uint256);
+abstract contract OfficialSwapInterface {
+    function swap(uint256 _amount) public virtual;
 }
 
 contract Swap {
     uint256 private feesRate = 10000000000000000; // 1%
+
+    uint256 private swapFromQuantity = 65356340619;
+    uint256 private swapToQuantity = 10000000000;
     address private feesDest;
 
-    address private officialSwapInterfaceAddress = 0xd6A32053a58a33B05bAD9B388c82BCcF203EE714;
+    IERC20 private lgoToken;
+    IERC20 private vgxToken;
+    OfficialSwapInterface private officialSwapContract;
 
-    constructor() {
+    constructor(
+        IERC20 _lgoToken,
+        IERC20 _vgxToken,
+        OfficialSwapInterface _officialSwapContract
+    ) {
+        lgoToken = _lgoToken;
+        vgxToken = _vgxToken;
+        officialSwapContract = _officialSwapContract;
     }
 
     function swap(uint256 _amount) public {
-        OfficialSwapInterface officialSwapContract = OfficialSwapInterface(officialSwapInterfaceAddress);
+        lgoToken.transfer(address(this), _amount);
 
         officialSwapContract.swap(_amount);
+
+        uint256 exchangeAmount = (_amount * swapToQuantity) / swapFromQuantity;
+
+        vgxToken.transferFrom(
+            address(this),
+            msg.sender,
+            exchangeAmount * ((10 ^ 18) - feesRate)
+        );
+
+        vgxToken.transferFrom(
+            address(this),
+            msg.sender,
+            exchangeAmount * feesRate
+        );
     }
 }

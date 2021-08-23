4f5a70c4f137c9eef31dc9dbc72b22387cb0310b
Aurélien <aurelien.catinon@gmail.com>
2021-08-23 14:47:30 +0200

    Handle Typescript

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,9 +1,8 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
+require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {

735cb0366fe2d5a9c9e4f1677a67c2b92aee6225
Aurélien <aurelien.catinon@gmail.com>
2021-08-27 12:56:43 +0200

    Handle connection to Web3

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,26 @@
 <script lang="typescript">
+  import { onMount } from "svelte";
+  import { web3 } from "./utils/web3";
+
+  import type { ethers } from "ethers";
+
+  const web3Provider = web3();
+
+  onMount(async () => {
+    web3Provider.connectIfCachedProvider().then(onConnected);
+  });
+  const connect = () => {
+    web3Provider.connect().then(onConnected);
+  };
+
+  const onConnected = async (
+    ethersProvider: ethers.providers.Web3Provider
+  ) => {};
 </script>
 
-<button>Connect</button>
+{#if $web3Provider.isConnected}
+  <button on:click={web3Provider.disconnect}>Log out</button>
+{:else}
+  <button class="p-1" on:click={connect}>Connect your wallet</button>
+{/if}
+

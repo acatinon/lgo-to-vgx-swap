03feb8a77b8d61e4e0088993f2e3d21e7a17957b
Aurélien <aurelien.catinon@gmail.com>
2021-09-02 14:18:05 +0200

    Using AutoNumeric.js to format number inputs

--- frontend/src/steps/Approve.svelte
+++ frontend/src/steps/Approve.svelte
@@ -1,16 +1,29 @@
 <script lang="typescript">
-  import { getContext } from "svelte";
+  import { onMount, getContext } from "svelte";
+  import BigNumber from "bignumber.js";
+  import AutoNumeric from "autonumeric";
   import type { Context } from "../context";
 
   let context: Context = getContext("context");
+  let approveInput: AutoNumeric;
+
+  onMount(async () => {
+    approveInput = new AutoNumeric("#approveAmount", 0, {
+      decimalPlaces: 8,
+      minimumValue: "0",
+      maximumValue: context.lgoBalance!.toString(),
+    });
+  });
 
   const approve = () => {
-    context!.lgo!.approve(
+    context.lgo!.approve(
       "0x0E801D84Fa97b50751Dbf25036d067dCf18858bF",
-      context.lgoBalance!
+      new BigNumber(approveInput!.getNumericString()!)
     );
   };
 </script>
 
 <p>{context.lgoBalance}</p>
+<input id="approveAmount" type="text" />
+<a href="#">Max</a>
 <button on:click={approve}>Approve</button>

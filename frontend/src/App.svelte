4e1b6c8fd30188d9a760eaf011d8a80b09ef19ae
Aurélien <aurelien.catinon@gmail.com>
2021-09-22 23:51:21 +0200

    Refactoring waiting steps

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,6 +9,7 @@
   import type { ethers } from "ethers";
 
   import Step from "./shared/Step.svelte";
+  import WaitingSteps from "./shared/WaitingSteps.svelte";
 
   let stepManager = new StepManager();
   let error: Error;
@@ -80,37 +81,14 @@
           <button class="block" on:click={approve}>Approve</button>
         </p>
       </Step>
-      <Step
-        title="Confirm the transaction on Metamask"
-        refState={State.WaitingApproveConfirmation}
+
+      <WaitingSteps
+        refConfirmationState={State.WaitingApproveConfirmation}
+        refExecutionState={State.WaitingApproveExecution}
         {currentState}
         {error}
-      >
-        <svg slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-          />
-        </svg>
-      </Step>
-      <Step
-        title="Wait for the transaction execution on the Ethereum blockchain"
-        refState={State.WaitingApproveExecution}
-        {currentState}
-        {error}
-      >
-        <svg slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-          />
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
-      </Step>
+      />
+
       <Step title="Swap" refState={State.Swapping} {currentState} {error}>
         <svg slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path
@@ -124,6 +102,13 @@
           The second step is the swap itself. Each step is a transaction on the Ethereum blockchain and thus cost fees.
         </p>
       </Step>
+
+      <WaitingSteps
+        refConfirmationState={State.WaitingSwapExecution}
+        refExecutionState={State.WaitingSwapExecution}
+        {currentState}
+        {error}
+      />
     {:else}
       <button class="m-auto block" on:click={connect}>Connect your wallet</button>
     {/if}
